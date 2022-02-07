f7264c469e5949d382cf795b845a09c58fe6e077
Harold Jeshua Varde <hjvarde@integritynet.biz>
2022-02-07 14:34:22 +0800

    lesson 20

--- add.php
+++ add.php
@@ -6,37 +6,48 @@
     //     echo $_GET['ingredients'];
     // }
 
+    $email = $title = $ingredients = '';
+    $errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
     if (isset($_POST['submit'])) {
                 
         // email check
         if (empty($_POST['email'])) {
-            echo 'Email is required <br/>';
+            $errors['email'] = 'Email is required <br/>';
         } else {
             $email = $_POST['email'];
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                echo 'Email must be a valid email address <br/>';
+                $errors['email'] = 'Email must be a valid email address <br/>';
             }
         }
        
         // title check
         if (empty($_POST['title'])) {
-            echo 'Title is required <br/>';
+            $errors['title'] = 'Title is required <br/>';
         } else {
             $title = $_POST['title'];
             if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-                echo 'Title must contain a word(s). <br/>';
+                $errors['title'] = 'Title must contain a word(s). <br/>';
             }
         }
        
         // ingredients check
         if (empty($_POST['ingredients'])) {
-            echo 'At least one ingredient is required <br/>';
+            $errors['ingredients'] = 'At least one ingredient is required <br/>';
         } else {
             $ingredients = $_POST['ingredients'];
             if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-                echo 'Ingredients must be comma separated list';
+                $errors['ingredients'] = 'Ingredients must be comma separated list';
             }
         }
+
+        if (array_filter($errors)) {
+            // echo 'form has errors';
+        } else {
+            // echo 'form is valid';
+            header('Lcoation: index.php');
+        }
+
     }
 
 ?>
@@ -51,13 +62,16 @@
 
         <form class="white" action="add.php" method="POST">
             <label for="email">Your Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"><?php echo $errors['email']; ?></div>
             
             <label for="title">Pizza Title:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <div class="red-text"><?php echo $errors['title']; ?></div>
 
             <label for="ingredients">Ingredients:</label>
-            <input type="text" name="ingredients">
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
 
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
--- index.php
+++ index.php
@@ -9,20 +9,4 @@
 
     <?php include 'templates/footer.php'; ?>
 
-
-    <!-- 
-    Updates:
-
-    iQA+AP:
-        - php tutorials @thenetninja (30/42):
-            - forms validation, filters, error checking + redirecting
-            - mysql/db
-            - data rendering
-            - explode function
-            - ..
-        - imported DB fo iQA
-        - quick debugging of apache errors
-
-
-     -->
 </html>
